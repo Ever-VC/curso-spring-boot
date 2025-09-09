6de1f9577d22b95f8c748b3e652c348617e5f40f
Ever-VC <ever.vasquezc27@gmail.com>
2025-09-08 19:30:34 -0600

    :sparkles: feat(UserRestController): Retornando JSOn pero usando DTO en una API simulada

--- src/main/java/com/ever/curso/springboot/webapp/springbootweb/controllers/UserRestController.java
+++ src/main/java/com/ever/curso/springboot/webapp/springbootweb/controllers/UserRestController.java
@@ -1,6 +1,7 @@
 package com.ever.curso.springboot.webapp.springbootweb.controllers;
 
 import com.ever.curso.springboot.webapp.springbootweb.models.User;
+import com.ever.curso.springboot.webapp.springbootweb.models.dto.UserDto;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +23,15 @@ public class UserRestController {
         body.put("lastName", "Vasquez");
         body.put("user", user);
         return body;
+    }
+
+    // Usando DTO en vez de Map
+    @GetMapping("/details-dto")
+    public UserDto detailsDto() {
+        User user = new User("Ever", "Vasquez");
+        UserDto userDto = new UserDto();
+        userDto.setTitle("Hola Mundo desde Spring Boot");
+        userDto.setUser(user);
+        return userDto;
     }
 }

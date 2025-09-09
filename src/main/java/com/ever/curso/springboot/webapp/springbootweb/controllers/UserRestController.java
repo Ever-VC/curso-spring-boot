0ae861f8c31f9f8b248b69604ee66ada9d1c2fc6
Ever-VC <ever.vasquezc27@gmail.com>
2025-09-08 19:45:29 -0600

    :sparkles: feat(UserRestController): Retornando un arreglo o lista de objetos de tipo usuario

--- src/main/java/com/ever/curso/springboot/webapp/springbootweb/controllers/UserRestController.java
+++ src/main/java/com/ever/curso/springboot/webapp/springbootweb/controllers/UserRestController.java
@@ -2,12 +2,13 @@ package com.ever.curso.springboot.webapp.springbootweb.controllers;
 
 import com.ever.curso.springboot.webapp.springbootweb.models.User;
 import com.ever.curso.springboot.webapp.springbootweb.models.dto.UserDto;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -23,6 +24,19 @@ public class UserRestController {
         body.put("lastName", "Vasquez");
         body.put("user", user);
         return body;
+    }
+
+    // Obtener lista de usuarios (sería lo mismo para arreglos pero con List es más común, en caso de usar arreglo sería User[])
+    @GetMapping("/users")
+    public List<User> list() {
+        User ever = new User("Ever", "Vasquez");
+        User juan = new User("Juan", "Perez");
+        User andres = new User("Andres", "Guzman");
+        User john = new User("John", "Doe");
+        // Crear una lista inmutable con List.of (Java 9+), funciona cuando ya tenemos los elementos
+        List<User> users = List.of(ever, juan, andres, john);
+        //List<User> users = Arrays.asList(ever, juan, andres, john); // Otra forma de crear una lista
+        return users;
     }
 
     // Usando DTO en vez de Map

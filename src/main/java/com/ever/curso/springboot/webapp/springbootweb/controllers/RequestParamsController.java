f999d627725cb264d6026436c9d9b89193915595
Ever-VC <ever.vasquezc27@gmail.com>
2025-09-16 20:06:21 -0600

    :sparkles: feat(RequestParamsController): Parametros con HttpServletRequest

--- src/main/java/com/ever/curso/springboot/webapp/springbootweb/controllers/RequestParamsController.java
+++ src/main/java/com/ever/curso/springboot/webapp/springbootweb/controllers/RequestParamsController.java
@@ -1,6 +1,7 @@
 package com.ever.curso.springboot.webapp.springbootweb.controllers;
 
 import com.ever.curso.springboot.webapp.springbootweb.models.dto.ParamDto;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,6 +23,20 @@ public class RequestParamsController {
     public ParamDto bar(@RequestParam String text, @RequestParam Integer code) {
         ParamDto paramsDto = new ParamDto();
         paramsDto.setMessage(text);
+        paramsDto.setCode(code);
+        return paramsDto;
+    }
+
+    @GetMapping("/request")
+    public ParamDto request(HttpServletRequest request) {
+        Integer code = 10;
+        ParamDto paramsDto = new ParamDto();
+        paramsDto.setMessage(request.getParameter("text"));
+        try {
+            code = Integer.parseInt(request.getParameter("code"));
+        } catch (NumberFormatException e) {
+
+        }
         paramsDto.setCode(code);
         return paramsDto;
     }
